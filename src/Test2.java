b9e7eda357f9e8713735b899c519fddcbb2a4b67
Rajesh <rajeshveldanda1993@gmail.com>
2023-07-21 16:43:39 +0530

    multiple data added

--- src/Test2.java
+++ src/Test2.java
@@ -1,11 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("dfndvn");
 		
 		System.out.println("Helo");
 		System.out.println("Bye");
